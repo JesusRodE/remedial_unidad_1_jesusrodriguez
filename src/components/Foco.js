27adde91f4121118259b78c8d1e27a699238e56b
Jesus Rodriguez <jesuse1661@gmail.com>
2022-02-24 19:57:05 -0700

    Programa que cambia el color de un foco y lo apaga

---
+++ src/components/Foco.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react'
+import {Image, View, Button} from 'react-native'
+
+const Foco =() =>{
+
+
+    const [estatus, setEstatus] = useState( require('../../img/focoOFF.png') );
+
+    return(
+        <View>
+            <Image source={ estatus }
+                style={{ width:300, height:300}}
+            />
+            <Button  title="Apagar" color="gray" onPress={()=>setEstatus(require('../../img/focoOFF.png'))} />
+            <Button  title="Amarillo" color="#FFBA1F" onPress={()=>setEstatus(require('../../img/focoOn.png'))} />
+            <Button  title="Rojo" color="red" onPress={()=>setEstatus(require('../../img/focoRojo.png'))} />
+            <Button  title="Azul" color="blue" onPress={()=>setEstatus(require('../../img/focoAzul.png'))} />
+            <Button  title="Verde" color="green" onPress={()=>setEstatus(require('../../img/focoVerde.png'))} />
+        </View>
+        
+    )
+}
+
+export default Foco;
