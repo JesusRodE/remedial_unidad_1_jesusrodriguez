9e8aa34195e2b69b80f36060f62ab794241a7462
JesusRodE <99356822+JesusRodE@users.noreply.github.com>
2022-02-24 20:05:03 -0700

    Update Foco.js

--- src/components/Foco.js
+++ src/components/Foco.js
@@ -4,14 +4,14 @@ import {Image, View, Button} from 'react-native'
 const Foco =() =>{
 
 
-    const [estatus, setEstatus] = useState( require('../../img/focoOFF.png') );
+    const [estatus, setEstatus] = useState( require('../../img/focoOff.png') );
 
     return(
         <View>
             <Image source={ estatus }
                 style={{ width:300, height:300}}
             />
-            <Button  title="Apagar" color="gray" onPress={()=>setEstatus(require('../../img/focoOFF.png'))} />
+            <Button  title="Apagar" color="gray" onPress={()=>setEstatus(require('../../img/focoOff.png'))} />
             <Button  title="Amarillo" color="#FFBA1F" onPress={()=>setEstatus(require('../../img/focoOn.png'))} />
             <Button  title="Rojo" color="red" onPress={()=>setEstatus(require('../../img/focoRojo.png'))} />
             <Button  title="Azul" color="blue" onPress={()=>setEstatus(require('../../img/focoAzul.png'))} />
